c0e61dafa74e5c64ae036814ddede8a84ef9157b
Arabprogrammer <Arabprogrammer3030@gmail.com>
2021-06-29 22:32:25 +0200

    project init

---
+++ src/main/java/com/app/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.app.config;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.app"))
+				.paths(PathSelectors.any()).build()
+				.apiInfo(getInfo());
+	}
+
+	private ApiInfo getInfo() {
+		Contact contact = new Contact("Bashir Janbalat", "https://www.facebook.com/", "Arabprogrammer3030@gmail.com");
+		return new ApiInfo("Spring Boot CRUD Project With Swagger", "Product Controller", "0.1", "https://swagger.io/",
+				contact, "Licence https://swagger.io/", "https://swagger.io", new ArrayList<>());
+	}
+	
+}
